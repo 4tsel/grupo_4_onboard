7051b8d7fc1a78b10d0be38a18ff2e63a2a8a7ec
4tsel <axelbnovo@gmail.com>
2020-09-15 20:29:37 -0300

    Agregada ruta de error - Creada vista de error

--- site/app.js
+++ site/app.js
@@ -8,12 +8,17 @@ app.set(`view engine`, `ejs`);
 // Puerto
 app.listen(3030, ()=>console.log(`Servidor iniciado en el puerto 3030`));
 
+// Error 404
+
+
+
 // Rutas requeridas
 const routeMain = require(`./routes/main.js`);
 const routeProducts = require(`./routes/products.js`);
 const routeUsers = require(`./routes/users.js`);
 const routeCarrito = require(`./routes/carrito.js`);
 const routeCategorias = require(`./routes/categorias.js`);
+const { route } = require("./routes/main.js");
 
 
 // Rutas principales
@@ -22,3 +27,8 @@ app.use(`/prod`, routeProducts);
 app.use(`/user`, routeUsers);
 app.use(`/cart`, routeCarrito);
 app.use(`/cat`, routeCategorias);
+
+// Errores
+app.use((req, res, next) =>{
+    res.status(404).render(`error.ejs`)
+})
