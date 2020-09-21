5da2e6c809fba92eafebd0ef7e34c261e27dc2cf
4tsel <axelbnovo@gmail.com>
2020-09-21 15:51:15 -0300

    Agregado dinamismo en el home

--- site/controllers/mainController.js
+++ site/controllers/mainController.js
@@ -1,11 +1,18 @@
 const express = require(`express`);
 const fs = require(`fs`);
 
+let productsDB = require(`../data/productDB.js`);
+
 const mainController = {
     index: (req, res) => {
+
+        
+
         res.render(`index.ejs`,
             {
                 titulo: `ONBOARD`,
+                productos: productsDB.productos,
+                categorias: productsDB.categorias
             });
     },
 }
