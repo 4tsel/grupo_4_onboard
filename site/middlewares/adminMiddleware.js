f4517b674f7e8307b1e291ab5606de592ca4ae07
4tsel <axelbnovo@gmail.com>
2020-10-06 12:20:06 -0300

    Vista de agregar categoría más intuitiva.

--- site/middlewares/adminMiddleware.js
+++ site/middlewares/adminMiddleware.js
@@ -1,7 +1,10 @@
-module.exports = (req, res, next)=>{
-    if(req.session.user.admin == true){
+module.exports = (req, res, next) => {
+    if (req.session.user != undefined && req.session.user.admin == true) {
+
         next()
     } else {
         res.render(`admin.ejs`)
     }
 }
+
+
